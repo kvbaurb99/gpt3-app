018760b62039a24dc94212e02877da5b0c254001
kvbaurb99 <kvbaurbanski2@gmail.com>
2023-04-03 23:37:53 +0200

    create chat button design

---
+++ src/components/utils/CreateChatButton.tsx
@@ -0,0 +1,12 @@
+import { BsPlus } from 'react-icons/bs'
+
+type Props = {}
+
+export default function CreateChatButton({}: Props) {
+  return (
+    <button className='text-white w-[95%] py-3 px-3 mt-2 ml-1 text-sm text-left rounded border border-gray-400/50 flex items-center hover:bg-gray-500/10 duration-300'>
+        <BsPlus className='text-xl' />
+        <p className='ml-2'>New Chat</p>
+    </button>
+  )
+}
