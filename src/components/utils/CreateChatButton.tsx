d121e563e7f60ea6058c3cf76f9fc0d51f5e1bc8
kvbaurb99 <kvbaurbanski2@gmail.com>
2023-04-04 09:17:20 +0200

    chat button design

--- src/components/utils/CreateChatButton.tsx
+++ src/components/utils/CreateChatButton.tsx
@@ -4,7 +4,7 @@ type Props = {}
 
 export default function CreateChatButton({}: Props) {
   return (
-    <button className='text-white w-[95%] py-3 px-3 mt-2 ml-1 text-sm text-left rounded border border-gray-400/50 flex items-center hover:bg-gray-500/10 duration-300'>
+    <button className='text-white w-[95%] py-3 px-3 mt-2 ml-1 text-sm text-left rounded-lg border border-gray-400/50 flex items-center hover:bg-gray-500/10 duration-300'>
         <BsPlus className='text-xl' />
         <p className='ml-2'>New Chat</p>
     </button>
