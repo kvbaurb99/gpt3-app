bf9decfaed9bf1a3dd6dfe9a7ade3a87ba2f4df2
kvbaurb99 <kvbaurbanski2@gmail.com>
2023-04-07 14:06:34 +0200

    dark/light mode

--- src/components/functionals/sidebar/options/LightMode.tsx
+++ src/components/functionals/sidebar/options/LightMode.tsx
@@ -1,12 +1,15 @@
-import { BsSun } from 'react-icons/bs'
+import { BsSun, BsMoon } from 'react-icons/bs'
 
-type Props = {}
+type Props = {
+  setIsDark: (isDark: boolean) => void,
+  isDark: boolean,
+}
 
-export default function LightMode({}: Props) {
+export default function LightMode({isDark, setIsDark}: Props) {
   return (
-    <div className='flex items-center gap-2 hover:bg-gray-700/30 py-3 px-2 rounded cursor-pointer'>
-        <BsSun />
-        <p>Light mode</p>
+    <div onClick={() => setIsDark(!isDark)} className='flex text-sm md:text-[15px] items-center gap-3 hover:bg-gray-700/30 py-3 px-2 rounded cursor-pointer'>
+        {isDark ? <BsSun /> : <BsMoon />}
+        {isDark ? <p>Light mode</p> : <p>Dark mode</p>}
     </div>
   )
 }
